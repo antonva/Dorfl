89a60ae8e003ae7e55b86383f6ff154177d9133c
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2014-01-28 00:16:38 +0000

    Update config.js
    Info vanish

--- modules/config.js
+++ modules/config.js
@@ -1,19 +1,19 @@
 //Config loading module for SpamBot.
 
 //Last.FM Module config variables.
-LFM_USER    = "azk-";
-LFM_SECRET  = "a247c6a0d4038e3d6b1e68f007ffce4b";
-LFM_API_KEY = "809561e04620e9163fdeaf5e3281ea0d";
+LFM_USER    = "";
+LFM_SECRET  = "";
+LFM_API_KEY = "";
 
 //IRC Module config variables. Found in ./modules/irc.js
-IRC_SERVER     = "ultra.root.is";
-IRC_PORT       = 61337;
+IRC_SERVER     = "";
+IRC_PORT       = 6667;
 IRC_SSL        = true;
 IRC_SELFSIGNED = true;
-IRC_CHANNELS   = ["#rass"];
-IRC_NICK       = "indridi";
-IRC_USERNAME   = "Indridi";
-IRC_REALNAME   = "Indriði Fóstbræðrason";
+IRC_CHANNELS   = ["#channel"];
+IRC_NICK       = "Dorfl";
+IRC_USERNAME   = "Dorfl";
+IRC_REALNAME   = "Dorfl";
 IRC_PASSWORD   = "";
 IRC_TRIGGER    = ".";
 
